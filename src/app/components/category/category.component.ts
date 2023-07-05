e5b057408c72a6ba50a53d94f4ccf9f9725692df
nurcantemur1 <nurtemur12@gmail.com>
2023-07-06 00:33:30 +0300

    aa

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -6,21 +6,20 @@ import { CategoryResponseModel } from 'src/app/models/categoryResponseModel';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
-export class CategoryComponent implements OnInit{
-
-  categories:Category[]=[];
-  apiUrl="https://localhost:44383/api/Categories/getall";
-  constructor(private httpClient:HttpClient){
-
+export class CategoryComponent implements OnInit {
+  categories: Category[] = [];
+  apiUrl = 'https://localhost:44383/api/Categories/getall';
+  constructor(private httpClient: HttpClient) {}
+  ngOnInit(): void {
+    this.getCategory();
   }
- ngOnInit(): void {
-   this.getCategory();
- }
-  getCategory(){
-    this.httpClient.get<CategoryResponseModel>(this.apiUrl).subscribe(responce=>{
-      this.categories=responce.data;
-    });
+  getCategory() {
+    this.httpClient
+      .get<CategoryResponseModel>(this.apiUrl)
+      .subscribe((responce) => {
+        this.categories = responce.data;
+      });
   }
 }
